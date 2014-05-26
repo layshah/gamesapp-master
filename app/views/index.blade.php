345cb7277a3c976ef87789e756695d1521c5e464
layshah <11bit057@nirmauni.ac.in>
2014-05-26 17:10:00 +0530

    neww
    mvd

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,7 +4,11 @@
     <div class="page-header">
         <h1>Interestship <small>home page catch 'em all!</small></h1>
     </div>
-
+  <div class="panel panel-default">
+        <div class="panel-body">
+            <a href="{{ action('GamesController@create') }}" class="btn btn-primary">Create Game</a>
+        </div>
+    </div>
     
 
     @if ($companys_personal_infos->isEmpty())
